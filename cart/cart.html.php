a6662bac9eabf34c003ae1ddb21f7ef06515fd2a
Dave-Lademan <dlademan94@gmail.com>
2017-04-26 18:21:00 -0400

    Add files via upload

---
+++ cart/cart.html.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Must Be Marian's</title>
+		<meta charset = "utf-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE = edge">
+    <meta name = "viewport" content = "width = device-width, initial-scale = 1">
+		<!--<link rel="stylesheet" type="text/css" href="style.css">-->
+		<link href = "../css/bootstrap.min.css" rel = "stylesheet">
+		<link href="../css/bootstrap-theme.min.css" rel="stylesheet">
+		<!-- Add in background styling based on  -->
+	</head>
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-12">
+					<nav class = "navbar navbar-default navbar-fixed-top " role = "navigation">
+						<div class = "navbar-header">
+							<button type = "button" class = "navbar-toggle"
+				         data-toggle = "collapse" data-target = "#example-navbar-collapse">
+				         <span class = "sr-only">Toggle navigation</span>
+				         <span class = "icon-bar"></span>
+				         <span class = "icon-bar"></span>
+				         <span class = "icon-bar"></span>
+				      </button>
+							<a class = "navbar-brand" href="/index.html"> <img src="../images/test-logo.png" class="img-responsive"  width="70" height="70" alt="logo"></a>
+						</div>
+						<div class = "collapse navbar-collapse" id = "example-navbar-collapse">
+							<ul class = "nav navbar-nav">
+								<li> <a href="../#home"> Home </a> </li>
+								<li> <a href="../#gallery"> Gallery </a> </li>
+								<li> <a href="../#products"> Products </a> </li>
+								<li> <a href="../#afterCare"> After Care </a> </li>
+								<li> <a href="../#aboutUs"> About Us </a> </li>
+							</ul>
+							<form class="navbar-form navbar-right">
+								<a href="#" class="btn btn-info btn-md active">
+				          <span class="glyphicon glyphicon-shopping-cart"></span>
+				        </a>
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Username">
+								</div>
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Password">
+								</div>
+								<button type="submit" class="btn btn-outline-success">Login</button>
+						</div>
+					</nav>
+				</div>
+			</div>
+			<div class="jumbotron" id="home" style="background-color:#2eb8b8">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-sm-12">
+							<br><h1> <strong>Cart</strong></h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="container-fluid">
+			<div class="jumbotron" style="background-color:#2eb8b8">
+				<div class="row">
+					<div class="col-sm-4">
+						<br><img src="../images/test-logo.png" class="img-rounded img-responsive"  width="200" height="200" alt="logo">
+					</div>
+					<div class="col-sm-8">
+						<br><h1> Footer Info here </h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src = "https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src = "../js/bootstrap.min.js"></script>
+	</body>
+</html>
